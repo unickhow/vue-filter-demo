caebdc7808d7cfafdc3bd2d0cf9d0c3b4a81abf0
Nick <unick.zhow@gmail.com>
2018-06-11 16:44:47 +0800

    add bike brief

---
+++ src/components/BikeBrief.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+	article.bike_brief
+		p {{bike.sna}} 
+			small {{bike.snaen}}
+		p {{bike.sarea}} 
+			small {{bike.sareaen}}
+		p {{bike.ar}} 
+			small {{bike.aren}}
+		p 可租借數：{{bike.sbi}}
+		p 可歸還數：{{bike.bemp}}
+</template>
+
+<script>
+	export default {
+		name: 'bike-brief',
+		props: ['bike'],
+		data() {
+			return {
+			}
+		},
+	}
+</script>
