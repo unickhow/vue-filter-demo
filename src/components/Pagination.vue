6352b448e9c411e9ee98c1468fe8bb59ac20d51c
Nick <unick.zhow@gmail.com>
2018-06-12 21:30:39 +0800

    pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -2,10 +2,25 @@
 	#pagination
 		ul
 			li(
+				@click='changePage(1)',
+				:class='{active: 1 === currentPage}')
+				a 1
+			li(v-if='currentPage >= 5 && totalPage >= 8')
+				a
+					i.fas.fa-ellipsis-h
+			li(
+				v-if='i !== 1 && i !== totalPage && ((totalPage > 8 && (currentPage - i <= 2 && i - currentPage <= 2)) || ( i <= 6 && currentPage <=4 ) || ( totalPage - i <= 5 && totalPage - currentPage <= 3 ))',
 				v-for='i in totalPage',
 				@click='changePage(i)',
-				:class='{active: i === $store.state.currentPage}')
+				:class='{active: i === currentPage}')
 				a {{i}}
+			li(v-if='totalPage - currentPage > 3 && totalPage >= 8')
+				a
+					i.fas.fa-ellipsis-h
+			li(
+				@click='changePage(totalPage)',
+				:class='{active: totalPage === currentPage}')
+				a {{totalPage}}
 </template>
 
 <script>
@@ -13,6 +28,9 @@
 		computed: {
 			totalPage() {
 				return this.$store.state.totalPage 
+			},
+			currentPage() {
+				return this.$store.state.currentPage 
 			}
 		},
 		methods: {
