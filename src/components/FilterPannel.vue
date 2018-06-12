6592021ac3027428e424c59ff9de9b9db917f619
Nick <unick.zhow@gmail.com>
2018-06-12 10:58:11 +0800

    filter checked status

--- src/components/FilterPannel.vue
+++ src/components/FilterPannel.vue
@@ -1,29 +1,46 @@
 <template lang="pug">
 	aside.filter_pannel
 		.area_filter
-			label 地區 
+			h6 地區 
 				small Area
 			ul
 				li(v-for='area in areaList')
-					i.far.fa-circle
-					label {{area.name_zh}} {{area.name_en}}
-						input(type='checkbox')
+					i.far(:class='area.isSelected ? "fa-check-circle" : "fa-circle"')
+					label {{area.name_zh}} 
+						small {{area.name_en}}
+						input(
+							type='checkbox',
+							v-model='area.isSelected')
 		.status_filter
-			label 使用狀態 
+			h6 使用狀態 
 				small Status
 			ul
 				li
-					i.far.fa-circle
-					label 可租借 Rentable
-						input(type='checkbox')
+					i.far(:class='rentable ? "fa-check-circle" : "fa-circle"')
+					label 可租借 
+						small Rentable
+						input(
+							type='checkbox',
+							v-model='rentable'
+							)
 				li
-					i.far.fa-check-circle
-					label 可歸還 Returnable
-						input(type='checkbox')
+					i.far(:class='returnable ? "fa-check-circle" : "fa-circle"')
+					label 可歸還 
+						small Returnable
+						input(
+							type='checkbox',
+							v-model='returnable'
+							)
 </template>
 <script>
 	export default {
 		name: 'filter-pannel',
+		data() {
+			return {
+				rentable: false,
+				returnable: false
+			}
+		},
 		computed: {
 			areaList() {
 				return this.$store.state.areaList 
--- src/store.js
+++ src/store.js
@@ -14,15 +14,19 @@ export const store = new Vuex.Store({
 		storeApiData(state, payload) {
 			state.bikeData = payload
 			state.isLoading = false
+
+			// find repeat object{} in array
 			payload = payload.filter((bike, index, self) =>
 				index === self.findIndex((t) => (
 					t.sarea === bike.sarea && t.sareaen === bike.sareaen
 				))
 			)
+
 			let areaArray = payload.map(bike => {
 				return {
 					name_zh: bike.sarea,
-					name_en: bike.sareaen
+					name_en: bike.sareaen,
+					isSelected: false
 				}
 			})
 			state.areaList = [...new Set(areaArray)]
