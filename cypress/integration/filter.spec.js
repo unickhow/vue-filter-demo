e490ba3f22f2d012190eedb5fb43775cdad8ab8a
Nick <unick.zhow@gmail.com>
2018-07-04 17:41:37 +0800

    more and more e2e test !

--- cypress/integration/filter.spec.js
+++ cypress/integration/filter.spec.js
@@ -20,10 +20,30 @@ context('Actions', () => {
 		cy.get('#map').should('not.be.visible')
 	})
 
-	// it('every area match results', () => {
-	// 	for(let i=0; i<12; i++) {
-	// 		let stopCounts = cy.get(':nth-child(1) > label > .counts')
-	// 		cy.get('.area_filter > ul > li > label').click()
-	// 	}
-	// })
+	it('every area match results', () => {
+		let count = 0
+		for(let i=0; i<12; i++) {
+			const currentArea = cy.get(`.area_filter > ul > li:nth-child(${i + 1}) > label`)
+			cy.get(`:nth-child(${i + 1}) > label > .counts`).then(($count) => {
+				count += parseInt($count.text())
+				currentArea.click()
+				cy.get('.filter_footer > p').should('have.text', `共 ${count} 個站點`)
+			})
+		}
+	})
+
+	it('clear tags', () => {
+		cy.get(`.area_filter > ul > li > label`).click({multiple: true})
+		cy.get('.all_tags > ul > li').should('have.length', 12)
+		cy.get('.filter_footer > a').should('be.visible')
+		cy.get('.filter_footer > a').click()
+		cy.get('.filter_footer > a').should('not.be.visible')
+	})
+
+	it('pagination pages', () => {
+		cy.get('.filter_footer > p').then(($totalCount) => {
+			const totalCount = $totalCount.text().split(' ')[1]
+			cy.get(':nth-child(8) > a').should('have.text', Math.ceil(totalCount/10).toString())
+		})
+	})
 })
